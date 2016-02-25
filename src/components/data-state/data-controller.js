70d8ccc1fb38eac2073a3b8a99f57da1795ca72c
Denis Lysenko <denis.lyisenko@qoderoom.com>
2016-02-25 10:39:51 +0200

    changes in data-service

--- src/components/data-state/data-controller.js
+++ src/components/data-state/data-controller.js
@@ -1,12 +1,11 @@
 class DataController {
 
 	constructor($location, DataService) {
-		var self = this;
 		this._$location = $location;
 
 		DataService.get()
-			.then(function(response) {
-				self.data = response.data;
+			.then((response) => {
+				this.data = response.data;
 			});
 	}
 }
